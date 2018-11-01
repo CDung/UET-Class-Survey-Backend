06930721ae1db1f5ccb0681e502fcf0c2906f09f
dungntc <nguyendung108108@gmail.com>
2018-11-01 16:20:52 +0700

    Added a Procfile.

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
-}).listen(8080);
+}).listen(process.env.PORT||8080);
