4a16f81fdfcc53909a77a2c266202368d7e6c1c9
dungntc <nguyendung108108@gmail.com>
2018-11-01 23:10:13 +0700

    ver-0

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express=require("express");
 var app=express();
+var fs=require("fs");
 var server=require("http").createServer(app);
 server.listen(process.env.PORT||1000);
+var dataIndex=fs.readFileSync(__dirname+"/index.html","utf-8");
 app.get("/",function (req,res) {
-    res.sendFile(__dirname+"/index.html");
+    res.send(dataIndex);
 });
 app.get("/index.html",function (req,res) {
-    res.sendFile(__dirname+"/index.html");
+    res.send(dataIndex);
 });
 app.get("/index.html/",function (req,res) {
-    res.sendFile(__dirname+"/index.html");
+    res.send(dataIndex);
 });
 app.get("*",function (req,res) {
   if (req.url.endsWith(".jpg")) {
