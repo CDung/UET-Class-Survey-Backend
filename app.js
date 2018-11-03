89ec3da2d597be5bf3532cafa31ba547ab73a09f
dungntc <nguyendung108108@gmail.com>
2018-11-03 23:25:25 +0700

    better

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
-const express = require('express');
-const app = express();
-const sercure=require('./control/sercure');
-const userController=require('./control/user');
-const jsonParser = require('body-parser').json();
+const express = require('express') 
+const app = express() 
+const sercure=require('./control/sercure') 
+const userController=require('./control/user') 
+const jsonParser = require('body-parser').json() 
 const cookieParser = require('cookie-parser')
 
-app.use(jsonParser);
+app.use(jsonParser) 
 app.use((req, res, next) => {   
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With,Authorization');
+    res.header('Access-Control-Allow-Origin', '*') 
+    res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With,Authorization') 
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    next();
-}); 
-app.use(express.static('data'));
-app.use(cookieParser());  
-app.listen(process.env.PORT ||3000);
+    next() 
+})  
+app.use(express.static('data')) 
+app.use(cookieParser())   
+app.listen(process.env.PORT ||3000) 
 
 
 app.post('/api/login', function(req, res){
-	sercure.authenticate(req,res);
-});
+	sercure.authenticate(req,res) 
+}) 
 
 app.get('/api/profile', sercure.verifyToken, (req, res) => {
-      userController.getProfile(req, res);
+      userController.getProfile(req, res) 
 })
+app.get('*',function(req,res){
+		if(req.url===""||req.url==="/"||req.url==="/index.html"||req.url==="/index.html/"||req.url==="/index"){
+		res.sendFile(__dirname+"/index.html")
+		return
+	}	
+	if (req.url.startsWith("/resource")&&req.url.endsWith(".jpg")) {
+    	res.sendFile(__dirname+req.url)
+    	return
+  	}
+  	res.send("404 not found")
+})
+const express = require('express') 
+const app = express() 
+const sercure=require('./control/sercure') 
+const userController=require('./control/user') 
+const jsonParser = require('body-parser').json() 
+const cookieParser = require('cookie-parser')
+
+app.use(jsonParser) 
+app.use((req, res, next) => {   
+    res.header('Access-Control-Allow-Origin', '*') 
+    res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With,Authorization') 
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    next() 
+})  
+app.use(express.static('data')) 
+app.use(cookieParser())   
+app.listen(process.env.PORT ||3000) 
 
 
+app.post('/api/login', function(req, res){
+	sercure.authenticate(req,res) 
+}) 
+
+app.get('/api/profile', sercure.verifyToken, (req, res) => {
+      userController.getProfile(req, res) 
+})
+app.get('*',function(req,res){
+		if(req.url===""||req.url==="/"||req.url==="/index.html"||req.url==="/index.html/"||req.url==="/index"){
+		res.sendFile(__dirname+"/index.html")
+		return
+	}	
+	if (req.url.startsWith("/resource")&&req.url.endsWith(".jpg")) {
+    	res.sendFile(__dirname+req.url)
+    	return
+  	}
+  	res.send("404 not found")
+})
